895d5e40f3d94c2b6d9e4583def4e4d82ddb1ceb
Dani <viodani01@gmail.com>
2020-01-17 14:57:00 -0500

    new branch

---
+++ Lab13_01.java
@@ -0,0 +1,14 @@
+//Daniela Agueros
+//Jesus Fernandez
+public class Lab13_01{
+   public static void main(String[]args){
+       new Lab13_01();
+   }
+   public Lab13_01(){
+       Square s1 = new Square();
+       Square s2 = new Square(5,5);
+       
+       System.out.println("Circle 1: " + s1);
+       System.out.println("Circle 2: " + s2);
+   }
+}
